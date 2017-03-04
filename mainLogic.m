13c31ba66cd962e1f3ed8057423121b4f388b2a0
Neonnsnow <tiankaixie@gmail.com>
2017-03-03 21:19:15 -0500

    Fill in the main logic
    fill in the main logic with while loop and some functions are added. Next is to implement the search function

--- mainLogic.m
+++ mainLogic.m
@@ -1,1 +1,31 @@
-mainLogic.m
+%% Recommander System
+fprintf('Welcome to Movie Recommender System ver.1.0!\n');
+
+fprintf('Initialization...\n');
+fprintf('Loading movie ratings dataset...\n');
+load ('ex8_movies.mat');
+fprintf('Done.\n');
+fprintf('For usage, please type \"help\"\n');
+
+command = '';
+
+%% Main while loop
+
+while (true) 
+  command = input('$ ','s');
+  if strcmp(command,'matrixImg')==1
+  	imagesc(Y),colorbar;
+  	ylabel('Movies');
+  	xlabel('Users');
+  elseif strcmp(command,'searchMovie')==1
+  	movieName = input('Please type the name of the movie:', 's');
+  	
+  elseif strcmp(command,'help')==1
+  	listDoc;
+  elseif strcmp(command,'quit')==1
+  	disp('Good Bye.');
+  	break;
+  end
+endwhile
+
+
---
+++ searchMovie.m
@@ -0,0 +1,3 @@
+function searchMovie(name)
+	[s, e, te, m, t, nm, sp] = regexp(movieList, name);
+endfunction
